51242b7b77a9b34587ce24d5b498c24b3cf1bf29
xRascalx <jk00242004@gmai.com>
2022-07-28 18:03:11 +0800

    MD5Utils 加密

--- src/main/java/com/lrm/blogbackend/service/UserServiceImpl.java
+++ src/main/java/com/lrm/blogbackend/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.lrm.blogbackend.service;
 
 import com.lrm.blogbackend.Repository.UserRepository;
 import com.lrm.blogbackend.entity.User;
+import com.lrm.blogbackend.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User checkUser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
+        User user = userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
         return user;
     }
 }
