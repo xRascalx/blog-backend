33137a32798f256869686b6303976cb150891543
xRascalx <jk00242004@gmai.com>
2022-08-02 17:58:14 +0800

    新增tag(error now)

--- src/main/java/com/lrm/blogbackend/web/admin/BlogController.java
+++ src/main/java/com/lrm/blogbackend/web/admin/BlogController.java
@@ -1,5 +1,6 @@
 package com.lrm.blogbackend.web.admin;
 
+import com.lrm.blogbackend.entity.Blog;
 import com.lrm.blogbackend.service.BlogService;
 import com.lrm.blogbackend.service.TypeService;
 import com.lrm.blogbackend.vo.BlogQuery;
@@ -12,12 +13,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+
 @Controller
 @RequestMapping("/admin")
 public class BlogController {
-
-    private final BlogService blogService;
+    private static final String INPUT = "admin/blogs-input";
+    private static final String LIST = "admin/blogs";
+    private static final String REDIRECT_LIST = "redirect:/admin/blogs";
+    private BlogService blogService;
     private TypeService typeService;
+
 
     public BlogController(BlogService blogService, TypeService typeService) {
         this.blogService = blogService;
@@ -29,12 +35,20 @@ public class BlogController {
     public String list(@PageableDefault(size = 2, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable, BlogQuery blog, Model model) {
         model.addAttribute("type",typeService.listType());
         model.addAttribute("page", blogService.listBlog(pageable, blog));
-        return "admin/blogs";
+        return LIST;
     }
 
     @PostMapping("/blogs/search")
     public String search(@PageableDefault(size = 2, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable, BlogQuery blog, Model model) {
         model.addAttribute("page", blogService.listBlog(pageable, blog));
         return "admin/blogs :: blogList";
+    }
+
+
+    @GetMapping("/blogs/input")
+    public String input(Model model){
+        model.addAttribute("type",typeService.listType());
+        model.addAttribute("blog", new Blog());
+        return INPUT;
     }
 }
