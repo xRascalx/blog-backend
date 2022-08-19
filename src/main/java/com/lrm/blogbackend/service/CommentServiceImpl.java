fe0da3ba32ff8751da077a8049ad682b8f4a5549
xRascalx <jk00242004@gmail.com>
2022-08-19 09:43:13 +0800

    留言-點擊回復可@某人

--- src/main/java/com/lrm/blogbackend/service/CommentServiceImpl.java
+++ src/main/java/com/lrm/blogbackend/service/CommentServiceImpl.java
@@ -2,15 +2,17 @@ package com.lrm.blogbackend.service;
 
 import com.lrm.blogbackend.Repository.CommentRepository;
 import com.lrm.blogbackend.entity.Comment;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Service
-public class CommentServiceImpl implements CommentService{
+public class CommentServiceImpl implements CommentService {
 
     private CommentRepository commentRepository;
 
@@ -20,8 +22,9 @@ public class CommentServiceImpl implements CommentService{
 
     @Override
     public List<Comment> listCommentByBlogId(Long blogId) {
-        Sort sort = Sort.by(Sort.Direction.DESC,"createTime");
-        return commentRepository.findByBlogId(blogId, sort);
+        Sort sort = Sort.by(Sort.Direction.DESC, "createTime");
+        List<Comment> comments = commentRepository.findByBlogIdAndParentCommentNull(blogId, sort);
+        return eachComment(comments);
     }
 
     @Transactional
@@ -29,12 +32,69 @@ public class CommentServiceImpl implements CommentService{
     public Comment saveComment(Comment comment) {
         //如果有回復的話，不等於-1的狀態去找到他的父級，否則就為空平常的保存
         Long parentCommentId = comment.getParentComment().getId();
-        if(parentCommentId != -1){
+        if (parentCommentId != -1) {
             comment.setParentComment(commentRepository.findById(parentCommentId).orElse(null));
         } else {
             comment.setParentComment(null);
         }
         comment.setCreateTime(new Date());
         return commentRepository.save(comment);
+    }
+
+    /**
+     * 循環每個頂級的評論節點
+     *
+      * @param comments
+     * @return
+     */
+    private List<Comment> eachComment(List<Comment> comments) {
+        List<Comment> commentsView = new ArrayList<>();
+        for (Comment comment : comments) {
+            Comment c = new Comment();
+            BeanUtils.copyProperties(comment, c);
+            commentsView.add(c);
+        }
+        //合併評論的各層子代到第一級子代集合中
+        combineChildren(commentsView);
+        return commentsView;
+    }
+
+    /**
+     * @param comments root節根點，blog不為空的對象集合
+     * @return
+     */
+    private void combineChildren(List<Comment> comments) {
+        for (Comment comment : comments) {
+            List<Comment> replys1 = comment.getReplyComments();
+            for (Comment reply1 : replys1) {
+                //循環迭代，找出子代，存放在tempReplys中
+                recursively(reply1);
+            }
+            //修改頂級節點的reply集合為迭代處理後的集合
+            comment.setReplyComments(tempReplys);
+            tempReplys = new ArrayList<>();
+        }
+    }
+
+    //存放迭代找出的所有子代的集合
+    private List<Comment> tempReplys = new ArrayList<>();
+
+    /**
+     * 递归迭代，剥洋葱
+     *
+     * @param comment 被迭代的对象
+     * @return
+     */
+    private void recursively(Comment comment) {
+        tempReplys.add(comment);//顶节点添加到临时存放集合
+        if (comment.getReplyComments().size() > 0) {
+            List<Comment> replys = comment.getReplyComments();
+            for (Comment reply : replys) {
+                tempReplys.add(reply);
+                if (reply.getReplyComments().size() > 0) {
+                    recursively(reply);
+                }
+            }
+        }
     }
 }
