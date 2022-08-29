9c894f5ed3bfd7a3b5ca5efbb21b9265f3aa1e00
xRascalx <jk00242004@gmail.com>
2022-08-29 11:16:30 +0800

    thymeleaf 判斷

--- src/main/java/com/lrm/blogbackend/web/BookController.java
+++ src/main/java/com/lrm/blogbackend/web/BookController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class BookController {
 
@@ -17,7 +19,9 @@ public class BookController {
     }
 
     @GetMapping("/allBook")
-    public String list(){
+    public String list(Model model){
+        List<Book> book = bookService.findAll();
+        model.addAttribute("book", book);
         return "books";
     }
 
