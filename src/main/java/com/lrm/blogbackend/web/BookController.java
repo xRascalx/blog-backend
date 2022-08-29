f4dda657a161e867b57830612d6b3de7789f0480
xRascalx <jk00242004@gmail.com>
2022-08-29 17:00:47 +0800

    thymeleaf redirect導頁，form傳參新增資料

--- src/main/java/com/lrm/blogbackend/web/BookController.java
+++ src/main/java/com/lrm/blogbackend/web/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -44,5 +45,25 @@ public class BookController {
         }
         model.addAttribute("book", book);
         return "book";
+    }
+
+    /**
+     * 跳轉input提交頁面
+     * @return
+     */
+    @GetMapping("/allBook/input")
+    public String inputPage(){
+        return "input";
+    }
+
+    /**
+     * 提交一個書單訊息
+     * @param book
+     * @return
+     */
+    @PostMapping("/allBook")
+    public String post(Book book){
+        bookService.save(book);
+        return "redirect:/allBook";
     }
 }
