e897da1ddc9b49b17ea13454d00f81324f08ee27
xRascalx <jk00242004@gmail.com>
2022-08-30 10:46:28 +0800

    thymeleaf 初始化表單數據(做更新用)

--- src/main/java/com/lrm/blogbackend/web/BookController.java
+++ src/main/java/com/lrm/blogbackend/web/BookController.java
@@ -21,11 +21,12 @@ public class BookController {
 
     /**
      * 獲取書單列表
+     *
      * @param model
      * @return
      */
     @GetMapping("/allBook")
-    public String list(Model model){
+    public String list(Model model) {
         List<Book> books = bookService.findAll();
         model.addAttribute("books", books);
         return "books";
@@ -33,36 +34,55 @@ public class BookController {
 
     /**
      * 獲取書單詳情
+     *
      * @param id
      * @param model
      * @return
      */
     @GetMapping("/allBook/{id}")
-    public String detail(@PathVariable long id, Model model){
+    public String detail(@PathVariable long id, Model model) {
         Book book = bookService.findOne(id);
-        if(book == null){
-            book = new Book();
-        }
+//        if (book == null) {
+//            book = new Book();
+//        }
         model.addAttribute("book", book);
         return "book";
     }
 
     /**
      * 跳轉input提交頁面
+     *
      * @return
      */
     @GetMapping("/allBook/input")
-    public String inputPage(){
+    public String inputPage(Model model) {
+        model.addAttribute("book", new Book());
         return "input";
     }
 
     /**
+     * 跳轉到更新頁面input
+     *
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping("/allBook/{id}/input")
+    public String inputEditPage(@PathVariable long id, Model model) {
+        Book book = bookService.findOne(id);
+        model.addAttribute("book", book);
+        return "input";
+    }
+
+
+    /**
      * 提交一個書單訊息
+     *
      * @param book
      * @return
      */
     @PostMapping("/allBook")
-    public String post(Book book){
+    public String post(Book book) {
         bookService.save(book);
         return "redirect:/allBook";
     }
